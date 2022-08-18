cb9a9427af8366f06d17b174408fbbc8d371e7d1
Chris Krygier <hoybyte@gmail.com>
2022-08-18 19:39:43 -0400

    updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,24 @@
-#fish shell config
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# Theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# Aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
