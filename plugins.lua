97e56a7a5ea70e333f19d7f1a24f1ed16b59b893
Chris Krygier <hoybyte@gmail.com>
2022-08-17 20:30:50 -0400

    add plugins.lua

---
+++ plugins.lua
@@ -0,0 +1,34 @@
+local status, packer = pcall(require, "packer")
+if (not status) then 
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'nvim-lualine/lualine.nvim' -- Status Line
+  use 'nvim-lua/plenary.nvim' -- Common Utilities
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovims's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovime/nvim-lspconfig' -- LSP
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  use 'glepnir/lspanga.nvim' -- LSP UIs
+  use 'L3MON4D3/LuaSnip'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  
+
