d8ab7f33dce2ebe6430ed278a5632abfab1c6378
hoybyte <hoybyte@gmail.com>
2022-08-19 15:41:02 -0400

    Added Fish functions and Updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -22,3 +22,35 @@ set -gx EDITOR nvim
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD -- description 'Do nvm stuff'
+   status --is-command-substitution; and return
+
+   if test -f .nvmrc; and test -r .nvmrc;
+      nvm use
+   else
+   end
+end
+
+switch (uname)
+case Darwin
+   source (dirname (status --current-filename))/config-osx.fish
+case Linux
+   source (dirname (status --current-filename))/config-linux.fish
+case '*'
+   source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-file))/config-local.fish
+if test -f $LOCAL_CONFIG
+   source $LOCAL_CONFIG
+end
+
